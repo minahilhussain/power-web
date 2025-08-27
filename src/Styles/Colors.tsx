3ab29fd5e6ffd046d95872c2710622e6dcc73e48
Minahil Hussain <minahilh21@gmail.com>
2025-08-27 20:40:36 +0500

    Added colors

---
+++ src/Styles/Colors.tsx
@@ -0,0 +1,13 @@
+const colors = {
+  SlateText: "#2E3E5C",
+  MutedText: "#637188",
+  Primary: "#706FE5",
+  PrimaryDark: "#5a58c7",
+  BrandNavy: "#1E1F4B",
+  White: "#fff",
+  SubtleLavender: "#EAE9FB",
+  PrimaryTint05: "#706FE505",
+  Black: "#000",
+};
+
+export default colors;
