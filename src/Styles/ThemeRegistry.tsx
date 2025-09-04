f4322a88f447ab6add112ce36ea65334635f3235
Minahil Hussain <minahilh21@gmail.com>
2025-09-04 22:04:24 +0500

    fixed hydration

--- src/Styles/ThemeRegistry.tsx
+++ src/Styles/ThemeRegistry.tsx
@@ -1,11 +1,21 @@
 "use client";
-
-import { PropsWithChildren } from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
+import { useEffect,useState } from "react";
+import createCache from "@emotion/cache";
 import theme from "./Theme";
 
-export default function ThemeRegistry({ children }: PropsWithChildren) {
+const clientSideCache = createCache({ key: "css", prepend: true });
+
+export default function ThemeRegistry({ children }: { children: React.ReactNode }) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
